f1b8e49b30b6335c88b651a667390298b21219a6
Vesa <90590852+veekooFIN@users.noreply.github.com>
2021-11-15 00:26:00 -0800

    Add files via upload

---
+++ lotto.c
@@ -0,0 +1,566 @@
+#include<stdio.h> 
+void main() 
+{ 
+  int a0[7],b0[5],c0[6],aa[7],bb[5],cc[6],i,ii,total;
+  int a1[7],b1[5],c1[6];
+  int a2[7],b2[5],c2[6];
+  int a3[7],b3[5],c3[6];
+  int a4[7],b4[5],c4[6];
+  int a5[7],b5[5],c5[6];
+  int a6[7],b6[5],c6[6];
+  int a7[7],b7[5],c7[6];
+  int a8[7],b8[5],c8[6];
+  int a9[7],b9[5],c9[6];
+   
+  printf("\nSyota vikinglotto numerot 6kpl 1-48 R1: "); 
+  for(i=0;i<6;i++) 
+  { 
+    scanf("%d",&c0[i]); 
+  }
+  printf("\nSyota vikinglotto numerot 6kpl 1-48 R2: "); 
+  for(i=0;i<6;i++) 
+  { 
+    scanf("%d",&c1[i]); 
+  }
+  printf("\nSyota vikinglotto numerot 6kpl 1-48 R3: "); 
+  for(i=0;i<6;i++) 
+  { 
+    scanf("%d",&c2[i]); 
+  }
+  printf("\nSyota vikinglotto numerot 6kpl 1-48 R4: "); 
+  for(i=0;i<6;i++) 
+  { 
+    scanf("%d",&c3[i]); 
+  }
+  printf("\nSyota vikinglotto numerot 6kpl 1-48 R5: "); 
+  for(i=0;i<6;i++) 
+  { 
+    scanf("%d",&c4[i]); 
+  }
+  printf("\nSyota vikinglotto numerot 6kpl 1-48 R6: "); 
+  for(i=0;i<6;i++) 
+  { 
+    scanf("%d",&c5[i]); 
+  }
+  printf("\nSyota vikinglotto numerot 6kpl 1-48 R7: "); 
+  for(i=0;i<6;i++) 
+  { 
+    scanf("%d",&c6[i]); 
+  }
+  printf("\nSyota vikinglotto numerot 6kpl 1-48 R8: "); 
+  for(i=0;i<6;i++) 
+  { 
+    scanf("%d",&c7[i]); 
+  }
+  printf("\nSyota vikinglotto numerot 6kpl 1-48 R9: "); 
+  for(i=0;i<6;i++) 
+  { 
+    scanf("%d",&c8[i]); 
+  }
+  printf("\nSyota vikinglotto numerot 6kpl 1-48 R10: "); 
+  for(i=0;i<6;i++) 
+  { 
+    scanf("%d",&c9[i]); 
+  }
+  
+  printf("\nSyota oikeat vikinglotto numerot 6kpl 1-48: "); 
+  for(i=0;i<6;i++) 
+  { 
+    scanf("%d",&cc[i]); 
+  } 
+   
+
+
+   
+  printf("\nSyota eurojackpot numerot 5kpl 1-50 R1: "); 
+  for(i=0;i<5;i++) 
+  { 
+    scanf("%d",&b0[i]); 
+  }
+  printf("\nSyota eurojackpot numerot 5kpl 1-50 R2: "); 
+  for(i=0;i<5;i++) 
+  { 
+    scanf("%d",&b1[i]); 
+  }
+  printf("\nSyota eurojackpot numerot 5kpl 1-50 R3: "); 
+  for(i=0;i<5;i++) 
+  { 
+    scanf("%d",&b2[i]); 
+  }
+  printf("\nSyota eurojackpot numerot 5kpl 1-50 R4: "); 
+  for(i=0;i<5;i++) 
+  { 
+    scanf("%d",&b3[i]); 
+  }
+  printf("\nSyota eurojackpot numerot 5kpl 1-50 R5: "); 
+  for(i=0;i<5;i++) 
+  { 
+    scanf("%d",&b4[i]); 
+  }
+  printf("\nSyota eurojackpot numerot 5kpl 1-50 R6: "); 
+  for(i=0;i<5;i++) 
+  { 
+    scanf("%d",&b5[i]); 
+  }
+  printf("\nSyota eurojackpot numerot 5kpl 1-50 R7: "); 
+  for(i=0;i<5;i++) 
+  { 
+    scanf("%d",&b6[i]); 
+  }
+  printf("\nSyota eurojackpot numerot 5kpl 1-50 R8: "); 
+  for(i=0;i<5;i++) 
+  { 
+    scanf("%d",&b7[i]); 
+  }
+  printf("\nSyota eurojackpot numerot 5kpl 1-50 R9: "); 
+  for(i=0;i<5;i++) 
+  { 
+    scanf("%d",&b8[i]); 
+  }
+  printf("\nSyota eurojackpot numerot 5kpl 1-50 R10: "); 
+  for(i=0;i<5;i++) 
+  { 
+    scanf("%d",&b9[i]); 
+  }
+  
+  printf("\nSyota oikeat eurojackpot numerot 5kpl 1-50: "); 
+  for(i=0;i<5;i++) 
+  { 
+    scanf("%d",&bb[i]); 
+  }
+
+
+
+  
+  printf("\nSyota lotto numerot 7kpl 1-40 R1: "); 
+  for(i=0;i<7;i++) 
+  { 
+    scanf("%d",&a0[i]); 
+  }
+  printf("\nSyota lotto numerot 7kpl 1-40 R2: "); 
+  for(i=0;i<7;i++) 
+  { 
+    scanf("%d",&a1[i]); 
+  }
+  printf("\nSyota lotto numerot 7kpl 1-40 R3: "); 
+  for(i=0;i<7;i++) 
+  { 
+    scanf("%d",&a2[i]); 
+  }
+  printf("\nSyota lotto numerot 7kpl 1-40 R4: "); 
+  for(i=0;i<7;i++) 
+  { 
+    scanf("%d",&a3[i]); 
+  }
+  printf("\nSyota lotto numerot 7kpl 1-40 R5: "); 
+  for(i=0;i<7;i++) 
+  { 
+    scanf("%d",&a4[i]); 
+  }
+  printf("\nSyota lotto numerot 7kpl 1-40 R6: "); 
+  for(i=0;i<7;i++) 
+  { 
+    scanf("%d",&a5[i]); 
+  }
+  printf("\nSyota lotto numerot 7kpl 1-40 R7: "); 
+  for(i=0;i<7;i++) 
+  { 
+    scanf("%d",&a6[i]); 
+  }
+  printf("\nSyota lotto numerot 7kpl 1-40 R8: "); 
+  for(i=0;i<7;i++) 
+  { 
+    scanf("%d",&a7[i]); 
+  }
+  printf("\nSyota lotto numerot 7kpl 1-40 R9: "); 
+  for(i=0;i<7;i++) 
+  { 
+    scanf("%d",&a8[i]); 
+  }
+  printf("\nSyota lotto numerot 7kpl 1-40 R10: "); 
+  for(i=0;i<7;i++) 
+  { 
+    scanf("%d",&a9[i]); 
+  }
+
+  printf("\nSyota oikeat lotto numerot 7kpl 1-40: "); 
+  for(i=0;i<7;i++) 
+  { 
+    scanf("%d",&aa[i]); 
+  }
+
+
+
+  
+  total=0; 
+  for(i=0;i<6;i++) 
+  { 
+    for(ii=0;ii<6;ii++) 
+    { 
+      if(cc[i]==c0[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nVikinglotto numeroita oikein R1:\t\t\t %d\n",total);
+  total=0; 
+  for(i=0;i<6;i++) 
+  { 
+    for(ii=0;ii<6;ii++) 
+    { 
+      if(cc[i]==c1[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nVikinglotto numeroita oikein R2:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<6;i++) 
+  { 
+    for(ii=0;ii<6;ii++) 
+    { 
+      if(cc[i]==c2[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nVikinglotto numeroita oikein R3:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<6;i++) 
+  { 
+    for(ii=0;ii<6;ii++) 
+    { 
+      if(cc[i]==c3[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nVikinglotto numeroita oikein R4:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<6;i++) 
+  { 
+    for(ii=0;ii<6;ii++) 
+    { 
+      if(cc[i]==c4[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nVikinglotto numeroita oikein R5:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<6;i++) 
+  { 
+    for(ii=0;ii<6;ii++) 
+    { 
+      if(cc[i]==c5[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nVikinglotto numeroita oikein R6:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<6;i++) 
+  { 
+    for(ii=0;ii<6;ii++) 
+    { 
+      if(cc[i]==c6[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nVikinglotto numeroita oikein R7:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<6;i++) 
+  { 
+    for(ii=0;ii<6;ii++) 
+    { 
+      if(cc[i]==c7[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nVikinglotto numeroita oikein R8:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<6;i++) 
+  { 
+    for(ii=0;ii<6;ii++) 
+    { 
+      if(cc[i]==c8[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nVikinglotto numeroita oikein R9:\t\t\t %d\n",total);  
+  total=0; 
+  for(i=0;i<6;i++) 
+  { 
+    for(ii=0;ii<6;ii++) 
+    { 
+      if(cc[i]==c9[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nVikinglotto numeroita oikein R10:\t\t\t %d\n",total);    
+
+
+
+
+  total=0; 
+  for(i=0;i<5;i++) 
+  { 
+    for(ii=0;ii<5;ii++) 
+    { 
+      if(bb[i]==b0[ii])
+      { 
+        total++;
+      }
+    }  
+  }   
+  printf("\nEurojackpot numeroita oikein R1:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<5;i++) 
+  { 
+    for(ii=0;ii<5;ii++) 
+    { 
+      if(bb[i]==b1[ii])
+      { 
+        total++;
+      }
+    }  
+  }   
+  printf("\nEurojackpot numeroita oikein R2:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<5;i++) 
+  { 
+    for(ii=0;ii<5;ii++) 
+    { 
+      if(bb[i]==b2[ii])
+      { 
+        total++;
+      }
+    }  
+  }   
+  printf("\nEurojackpot numeroita oikein R3:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<5;i++) 
+  { 
+    for(ii=0;ii<5;ii++) 
+    { 
+      if(bb[i]==b3[ii])
+      { 
+        total++;
+      }
+    }  
+  }   
+  printf("\nEurojackpot numeroita oikein R4:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<5;i++) 
+  { 
+    for(ii=0;ii<5;ii++) 
+    { 
+      if(bb[i]==b4[ii])
+      { 
+        total++;
+      }
+    }  
+  }   
+  printf("\nEurojackpot numeroita oikein R5:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<5;i++) 
+  { 
+    for(ii=0;ii<5;ii++) 
+    { 
+      if(bb[i]==b5[ii])
+      { 
+        total++;
+      }
+    }  
+  }   
+  printf("\nEurojackpot numeroita oikein R6:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<5;i++) 
+  { 
+    for(ii=0;ii<5;ii++) 
+    { 
+      if(bb[i]==b6[ii])
+      { 
+        total++;
+      }
+    }  
+  }   
+  printf("\nEurojackpot numeroita oikein R7:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<5;i++) 
+  { 
+    for(ii=0;ii<5;ii++) 
+    { 
+      if(bb[i]==b7[ii])
+      { 
+        total++;
+      }
+    }  
+  }   
+  printf("\nEurojackpot numeroita oikein R8:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<5;i++) 
+  { 
+    for(ii=0;ii<5;ii++) 
+    { 
+      if(bb[i]==b8[ii])
+      { 
+        total++;
+      }
+    }  
+  }   
+  printf("\nEurojackpot numeroita oikein R9:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<5;i++) 
+  { 
+    for(ii=0;ii<5;ii++) 
+    { 
+      if(bb[i]==b9[ii])
+      { 
+        total++;
+      }
+    }  
+  }   
+  printf("\nEurojackpot numeroita oikein R10:\t\t\t %d\n",total); 
+
+
+ 
+    
+  total=0; 
+  for(i=0;i<7;i++) 
+  { 
+    for(ii=0;ii<7;ii++) 
+    { 
+      if(aa[i]==a0[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nLotto numeroita oikein R1:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<7;i++) 
+  { 
+    for(ii=0;ii<7;ii++) 
+    { 
+      if(aa[i]==a1[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nLotto numeroita oikein R2:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<7;i++) 
+  { 
+    for(ii=0;ii<7;ii++) 
+    { 
+      if(aa[i]==a2[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nLotto numeroita oikein R3:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<7;i++) 
+  { 
+    for(ii=0;ii<7;ii++) 
+    { 
+      if(aa[i]==a3[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nLotto numeroita oikein R4:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<7;i++) 
+  { 
+    for(ii=0;ii<7;ii++) 
+    { 
+      if(aa[i]==a4[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nLotto numeroita oikein R5:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<7;i++) 
+  { 
+    for(ii=0;ii<7;ii++) 
+    { 
+      if(aa[i]==a5[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nLotto numeroita oikein R6:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<7;i++) 
+  { 
+    for(ii=0;ii<7;ii++) 
+    { 
+      if(aa[i]==a6[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nLotto numeroita oikein R7:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<7;i++) 
+  { 
+    for(ii=0;ii<7;ii++) 
+    { 
+      if(aa[i]==a7[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nLotto numeroita oikein R8:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<7;i++) 
+  { 
+    for(ii=0;ii<7;ii++) 
+    { 
+      if(aa[i]==a8[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nLotto numeroita oikein R9:\t\t\t %d\n",total); 
+  total=0; 
+  for(i=0;i<7;i++) 
+  { 
+    for(ii=0;ii<7;ii++) 
+    { 
+      if(aa[i]==a9[ii])
+      { 
+        total++;
+      }
+    }  
+  } 
+  printf("\nLotto numeroita oikein R10:\t\t\t %d\n",total); 
+  
+} 
+
+
